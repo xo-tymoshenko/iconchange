9fa76f6540f756354ec96aad2c3d9d02548043f4
xo-tymoshenko <xo.tymoshenko@gmail.com>
2024-11-03 09:51:53 +0100

    Added splash dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "dev.tymoshenko.iconchange"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "dev.tymoshenko.iconchange"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -56,4 +56,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Splash
+    implementation(libs.androidx.core.splashscreen)
 }
